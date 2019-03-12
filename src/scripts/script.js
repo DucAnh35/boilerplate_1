f3bf2d860b150ea1e4bf322eee77414139ec2208
DucAnh35 <nducanh3597@gmail.com>
2019-03-12 22:18:41 +0700

    css slice-slider

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,23 +1,16 @@
-$(document).ready(function () {
-    $(window).scroll(function () {
-        var scroll = $(window).scrollTop();
-        var opacity = 0;
-        if (scroll < 100) {
-            opacity = scroll / 100 ;
-            $("#b-header-container").css('background-color', 'rgba(203,190,190, ' + opacity + ')');
-        } else {
-            opacity = 1;
-            $("#b-header-container").css('background-color', 'rgba(203,190,190, ' + opacity + ')')
-        }
-    })
-});
 
-$(document).on('ready', function () {
-   
-    $(".lazy-item-1").slick({
-        lazyLoad: 'ondemand', // ondemand progressive anticipated
-        infinite: true,
-        dots: true,
-    });
-    
+$(document).ready(function () {
+    $(window)
+        .scroll(function () {
+            var scroll = $(window).scrollTop();
+            var opacity = 0;
+            if (scroll < 100) {
+                opacity = scroll / 100;
+                $("#b-header-container").css('background-color', 'rgba(203,190,190, ' + opacity + ')');
+            } else {
+                opacity = 1;
+                $("#b-header-container").css('background-color', 'rgba(203,190,190, ' + opacity + ')')
+            }
+        })
+    $('.mySlider').slick({ dots: true, infinite: true, arrows: false });
 });
