27cbafa3ae3730780faf638fb87c8e8d1bef976c
DucAnh35 <nducanh3597@gmail.com>
2019-03-13 23:03:55 +0700

    css slice-number

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -13,4 +13,12 @@ $(document).ready(function () {
             }
         })
     $('.mySlider').slick({ dots: true, infinite: true, arrows: false });
+    
+});
+
+jQuery(document).ready(function($) {
+    $('.counter').counterUp({
+        delay: 10,
+        time: 5000
+    });
 });
